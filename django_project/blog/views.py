ecafa504cc049fdf752cf189eb2e215aad932229
Emilian Ognianov <emilian_89@abv.bg>
2019-12-16 01:00:25 +0200

    adding layout

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'eognianov',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello</h>")
+    context = {
+        'posts' : posts,
+        'title' : 'Home'
+    }
+    return render(request, 'blog/home.html', context=context)
 
 def about(request):
-    return HttpResponse("<h3>About</h3>")
+    return render(request, 'blog/about.html', {'title': 'About'})
